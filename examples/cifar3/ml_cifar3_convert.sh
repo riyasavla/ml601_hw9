d1353c6698868e8d0b73730c699ba2066062d40e
riyasavla <rds@andrew.cmu.edu>
2017-04-30 23:07:30 -0400

    Add image mean into convert script

--- examples/cifar3/ml_cifar3_convert.sh
+++ examples/cifar3/ml_cifar3_convert.sh
@@ -3,3 +3,10 @@
 rm -rf cifar3_*
 
 python ml_cifar3_convert.py
+
+CAFFE_ROOT=/Users/riya/caffe
+TOOLS=build/tools
+EXAMPLE=examples/cifar3
+
+$CAFFE_ROOT/$TOOLS/compute_image_mean $CAFFE_ROOT/$EXAMPLE/cifar3_train_lmdb \
+  $CAFFE_ROOT/$EXAMPLE/mean.binaryproto
